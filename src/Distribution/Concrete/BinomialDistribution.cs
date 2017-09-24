8313f90e8f83fc523ebc5c4735ed47f1f3767235
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-09-24 14:30:10 +0200

    Add binomial distribution

---
+++ src/Distribution/Concrete/BinomialDistribution.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace RandomDistribution.Distribution.Concrete
+{
+    /// <summary>
+    /// Distribution class for a binomial distribution.
+    /// </summary>
+    /// <seealso cref="RandomDistribution.Distribution.IDistribution" />
+    public class BinomialDistribution : IDistribution
+    {
+        /// <summary>
+        /// Gets the p value of the distribution.
+        /// </summary>
+        /// <value>
+        /// The p value of the distribution.
+        /// </value>
+        public double P { get; }
+
+        /// <summary>
+        /// Gets the n value of the distribution.
+        /// </summary>
+        /// <value>
+        /// The n value of the distribution.
+        /// </value>
+        public int N { get; }
+
+        private readonly Random _rnd;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BinomialDistribution"/> class.
+        /// </summary>
+        /// <param name="p">The p value of the distribution.</param>
+        /// <param name="n">The n value of the distribution.</param>
+        public BinomialDistribution(double p, int n)
+        {
+            P = p;
+            N = n;
+            _rnd = new Random();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BinomialDistribution"/> class.
+        /// </summary>
+        /// <param name="p">The p value of the distribution.</param>
+        /// <param name="n">The n value of the distribution.</param>
+        /// <param name="seed">The seed of the distribution.</param>
+        public BinomialDistribution(double p, int n, int seed)
+        {
+            P = p;
+            N = n;
+            _rnd = new Random(seed);
+        }
+
+        /// <summary>
+        /// Gets next value.
+        /// </summary>
+        /// <returns>
+        /// Random value of said distribution.
+        /// </returns>
+        public double Next()
+        {
+            int value = 0;
+
+            for (int i = 0; i < N; ++i)
+            {
+                if (_rnd.NextDouble() < P)
+                {
+                    ++value;
+                }
+            }
+
+            return value;
+        }
+    }
+}

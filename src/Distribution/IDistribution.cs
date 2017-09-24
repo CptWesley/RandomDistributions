b81e61a1b0b8be4535f03bda500f68c8cc540160
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-09-24 11:42:56 +0200

    Add distribution interface

---
+++ src/Distribution/IDistribution.cs
@@ -0,0 +1,14 @@
+﻿namespace RandomDistribution.Distribution
+{
+    /// <summary>
+    /// Interface for distribution classes.
+    /// </summary>
+    public interface IDistribution
+    {
+        /// <summary>
+        /// Gets next value.
+        /// </summary>
+        /// <returns>Random value of said distribution.</returns>
+        double Next();
+    }
+}

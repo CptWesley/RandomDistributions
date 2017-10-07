857fc75a131aa8e60fdd1054c7cafdf3fa635d25
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-10-07 10:57:28 +0200

    Add Rademacher distribution

---
+++ src/Distribution/Discrete/RademacherDistribution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace RandomDistribution.Distribution.Discrete
+{
+    /// <summary>
+    /// Distribution class for a Rademacher distribution.
+    /// </summary>
+    /// <seealso cref="RandomDistribution.Distribution.IDistribution" />
+    /// <author>Wesley Baartman</author>
+    public sealed class RademacherDistribution : IDistribution
+    {
+        private readonly Random _rnd;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RademacherDistribution"/> class.
+        /// </summary>
+        public RademacherDistribution()
+        {
+            _rnd = new Random();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RademacherDistribution"/> class.
+        /// </summary>
+        /// <param name="seed">The seed of the distribution.</param>
+        public RademacherDistribution(int seed)
+        {
+            _rnd = new Random(seed);
+        }
+
+        /// <summary>
+        /// Gets next value.
+        /// </summary>
+        /// <returns>
+        /// Random value of said distribution.
+        /// </returns>
+        public double Next()
+        {
+            if (_rnd.NextDouble() > 0.5)
+            {
+                return 1;
+            }
+            return -1;
+        }
+    }
+}
